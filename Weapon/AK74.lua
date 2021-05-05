04417b3bf9353f076e949d80238306ae2abb2093
Козлов Никита <63429080+Nikitacauselove@users.noreply.github.com>
2021-05-05 16:18:43 +0300

    Update AK74.lua

--- Weapon/AK74.lua
+++ Weapon/AK74.lua
@@ -5,21 +5,21 @@ local isPointShooting = null;
 local isHipFire = null;	
 function OnEvent(event, arg)
 	if(event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
-		hasOptic = not  hasOptic;
+		hasOptic = not hasOptic;
 	end
 	if(event == "MOUSE_BUTTON_PRESSED" and arg == 6) then
-		isReady = not  isReady;
+		isReady = not isReady;
 	end
 	if(isReady and event == "MOUSE_BUTTON_PRESSED" and arg == 2) then
 		isPointShooting = false;
-		isHipFire =true;  
+		isHipFire = true;  
 	end
 	if(isReady and event == "MOUSE_BUTTON_RELEASED" and arg == 2) then
 		isPointShooting = true;
 		isHipFire = false;
 	end
 	if(hasOptic) then
-		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then
+		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
 			Sleep(math.random(36, 38));  
 			MoveMouseRelative(1, 6);   
 			while IsMouseButtonPressed(1) do  
@@ -30,12 +30,12 @@ function OnEvent(event, arg)
 				end 	 
 			end
 		end
-		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then
+		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
 			Sleep(math.random(36, 38));	       
 			MoveMouseRelative(0, 65);   
 		end
 	else
-		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then
+		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
 			Sleep(math.random(36, 38));  
 			MoveMouseRelative(1, 6);   
 			while IsMouseButtonPressed(1) do  
@@ -46,7 +46,7 @@ function OnEvent(event, arg)
 				end 	 
 			end
 		end
-		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then
+		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
 			Sleep(math.random(36, 38));	     
 			MoveMouseRelative(2, 11);   
 			while IsMouseButtonPressed(1) do   
