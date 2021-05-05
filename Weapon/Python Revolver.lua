d771f33e78acdbc6708c0fbf53f534c4f34c4c57
Козлов Никита <63429080+Nikitacauselove@users.noreply.github.com>
2021-05-06 00:49:06 +0300

    Add files via upload

---
+++ Weapon/Python Revolver.lua
@@ -0,0 +1,36 @@
+EnablePrimaryMouseButtonEvents(true);
+local isReady = false;
+local isPointShooting = null;
+function OnEvent(event, arg)
+    	if(event == "MOUSE_BUTTON_PRESSED" and arg == 6) then
+        	isReady = not isReady;
+    	end
+	if(isReady and event == "MOUSE_BUTTON_PRESSED" and arg == 2) then
+		isPointShooting = true; 
+	end
+	if(isReady and event == "MOUSE_BUTTON_RELEASED" and arg == 2) then
+		isPointShooting = false;
+	end
+   	if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    		Sleep(math.random(9, 11));
+		MoveMouseRelative(0, math.random(6, 8));
+    	end
+end
