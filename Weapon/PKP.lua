5dfafbe1bb4f2a8b9b7e4b928ac968cc21c6cdbe
Козлов Никита <63429080+Nikitacauselove@users.noreply.github.com>
2021-05-05 16:21:09 +0300

    Update PKP.lua

--- Weapon/PKP.lua
+++ Weapon/PKP.lua
@@ -5,10 +5,10 @@ local isPointShooting = null;
 local isHipFire = null;	
 function OnEvent(event, arg)
 	if(event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
-		hasOptic = not  hasOptic;
+		hasOptic = not hasOptic;
 	end
 	if(event == "MOUSE_BUTTON_PRESSED" and arg == 6) then
-		isReady = not  isReady;
+		isReady = not isReady;
 	end
 	if(isReady and event == "MOUSE_BUTTON_PRESSED" and arg == 2) then
 		isPointShooting = false;
@@ -19,7 +19,7 @@ function OnEvent(event, arg)
 		isHipFire = false; 
 	end
 	if(hasOptic) then
-		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then
+		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
 			Sleep(math.random(42, 44));	     
 			MoveMouseRelative(1, 4);    
 			while IsMouseButtonPressed(1) do   
@@ -40,12 +40,12 @@ function OnEvent(event, arg)
 					end 	
 			end 
 		end
-		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then
+		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
 			Sleep(math.random(32, 34));        
 			MoveMouseRelative(5, 59);          
 		end
 	else
-		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then
+		if(isReady and isHipFire and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
 			Sleep(math.random(42, 44));	     
 			MoveMouseRelative(1, 4);    
 			while IsMouseButtonPressed(1) do   
@@ -66,7 +66,7 @@ function OnEvent(event, arg)
 					end 	
 			end 
 		end
-		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg ==1) then 
+		if(isReady and isPointShooting and event == "MOUSE_BUTTON_PRESSED" and arg == 1) then 
 			Sleep(math.random(32, 34));      
 			MoveMouseRelative(1, 8);       
 			while IsMouseButtonPressed(1) do   
